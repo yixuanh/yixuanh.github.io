62c35940564981d7bdb5ded8557ae97696afb0ec
yixuanh <54297194+yixuanh@users.noreply.github.com>
2025-03-24 17:32:55 -0500

    Create about_siqi.d

---
+++ _pages/about_siqi.d
@@ -0,0 +1,3 @@
+**<span style="font-size: 1.5em;">Siqi Yang</span>**  
+<span style="color: #007acc;"><i>Masters Student</i></span>  
+Siqi is currently pursuing a Master of Public Health (MPH) with a concentration in Epidemiology and Biostatistics at Boston University School of Public Health. She is passionate about conducting research that applies statistical modeling to predict disease risk and outcomes in areas such chronic diseases and infectious disease. Siqi enjoys working with clinical, genetic, and real-world data to better understand health outcomes in diverse populations
